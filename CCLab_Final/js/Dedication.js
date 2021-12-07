b7e75d119a89339d30a4bf33b3c8b430ce53036f
Louis <zl3608@nyu.edu>
2021-12-07 22:30:45 +0800

    update2

---
+++ CCLab_Final/js/Dedication.js
@@ -0,0 +1,29 @@
+setTimeout(function() {
+   let link = document.getElementById("b1");
+   link.style.display="block";
+}, 2500);
+
+setTimeout(function() {
+   let link = document.getElementById("b2");
+   link.style.display="block";
+}, 3500);
+
+setTimeout(function() {
+   let link = document.getElementById("b3");
+   link.style.display="block";
+}, 4500);
+
+setTimeout(function() {
+   let link = document.getElementById("b4");
+   link.style.display="block";
+}, 5500);
+
+setTimeout(function() {
+   let link = document.getElementById("c1");
+   link.style.display="block";
+}, 1500);
+
+setTimeout(function() {
+   let link = document.getElementById("c2");
+   link.style.display="block";
+}, 1500);
